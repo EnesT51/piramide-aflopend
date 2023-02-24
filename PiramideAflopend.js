a2864ed4499a42bd11ad7b52590fb661326fa2d9
EnesT51 <99040669@mydavinci.nl>
2023-02-24 12:02:20 +0100

    opdr afgerond

--- PiramideAflopend.js
+++ PiramideAflopend.js
@@ -1,14 +1,29 @@
 function PiramideAflopend(){
-   var getal = prompt("Voer een getal in:");
+
+   var array = [];
+   var getal = prompt("Voer een Getal in: ");
+   for(let i = 0; i <= getal; i++){
+      array.push(i);
+   }
+   var title = document.createElement("h2");
+   var paragraph = document.createElement("p");
+   var para = document.createElement("p");
+   var node = document.createTextNode("Piramide Aflopend");
+
+   para.innerHTML = "Array: " + array;
+   title.appendChild(node);
+   document.body.appendChild(title);
+   document.body.appendChild(para);
+   document.body.appendChild(paragraph);
    if(getal == null){
       alert("Input is leeg, voer een getal in.");
       PiramideAflopend();
    }
    for(let i = getal; i >= 0; i--){
       for(let x = 0; x <= i; x++){
-         document.getElementById("PiramideAflopend").innerHTML += x;
+         paragraph.innerHTML += array[x];
       };
-      document.getElementById("PiramideAflopend").innerHTML += "<br>";
+      paragraph.innerHTML += "<br>";
    };
 };
 PiramideAflopend();
